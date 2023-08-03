716049e4a6d53de6a2ff976c46c855b061f24bfa
goswamiakshay <44131661+goswamiakshay@users.noreply.github.com>
2023-08-03 18:42:35 +0530

    Add files via upload

---
+++ drinks.java
@@ -0,0 +1,26 @@
+package Akshay;
+
+public class drinks extends Food_Ordering {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	public void drinks() 
+	{
+		A="Coke";
+		B="Thumbs-up";
+		C="Sprite";
+		D="Mountain Dew";
+		pr1=40;
+		pr2=40;
+		pr3=40;
+		pr4=40;
+		System.out.println("DRINKS");
+		System.out.println("Item A:"+A+"\t\tRs."+pr1);
+		System.out.println("Item B:"+B+"\tRs."+pr2);
+		System.out.println("Item C:"+C+"\t\tRs."+pr3);
+		System.out.println("Item D:"+D+"\tRs."+pr4);
+		select_menu_item();
+	}
+
+}
