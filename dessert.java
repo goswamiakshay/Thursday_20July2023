9686d5924b228fe48ff21e38adc99568310d6245
goswamiakshay <44131661+goswamiakshay@users.noreply.github.com>
2023-08-03 18:42:05 +0530

    Add files via upload

---
+++ dessert.java
@@ -0,0 +1,26 @@
+package Akshay;
+
+public class dessert extends Food_Ordering {
+
+	public static void main(String[] args) {
+
+	}
+	public void dessert()
+	{
+		A="Special Kheer";
+		B="Falooda";
+		C="Sitafal Cream";
+		D="Dry Fruit Milk Shake";
+		pr1=80;
+		pr2=150;
+		pr3=180;
+		pr4=200;
+		System.out.println("DESSERTS");
+		System.out.println("Item A:"+A+"\t\tRs."+pr1);
+		System.out.println("Item B:"+B+"\t\t\tRs."+pr2);
+		System.out.println("Item C:"+C+"\t\tRs."+pr3);
+		System.out.println("Item D:"+D+"\tRs."+pr4);
+		select_menu_item();
+	}
+
+}
