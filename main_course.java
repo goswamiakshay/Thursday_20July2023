12f0f6d5040056e0bdbfdd9f0bf70e2a7d240f78
goswamiakshay <44131661+goswamiakshay@users.noreply.github.com>
2023-08-03 18:43:03 +0530

    Add files via upload

---
+++ main_course.java
@@ -0,0 +1,26 @@
+package Akshay;
+
+public class main_course extends Food_Ordering {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	public void main_course()
+	{
+		A="Angara Kabab";
+		B="Chicken Double Ekka";
+		C="Chicken Behtareen";
+		D="Chicken Lajawab Kabab";
+		pr1=550;
+		pr2=550;
+		pr3=550;
+		pr4=500;
+		System.out.println("MAIN COURSE");
+		System.out.println("Item A:"+A+"\t\tRs."+pr1);
+		System.out.println("Item B:"+B+"\tRs."+pr2);
+		System.out.println("Item C:"+C+"\tRs."+pr3);
+		System.out.println("Item D:"+D+"\tRs."+pr4);
+		select_menu_item();
+	}
+
+}
